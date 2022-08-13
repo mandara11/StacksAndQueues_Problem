b9b099eb33e0044e40e4559db9072bd7dbe1109a
mandara11 <mandaram2345@gmail.com>
2022-08-13 21:47:27 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueues
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Problem");
+        }
+    }
+}
