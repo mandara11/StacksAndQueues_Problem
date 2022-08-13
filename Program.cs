404600fe0c303e7ff5f8870861805032fac7ee5c
mandara11 <mandaram2345@gmail.com>
2022-08-13 21:58:10 +0530

    Added:UC1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace StackAndQueues
+﻿using StacksAndQueues;
+
+namespace StackAndQueues
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue Problem");
+            Stack.CreateStack();
         }
     }
 }
