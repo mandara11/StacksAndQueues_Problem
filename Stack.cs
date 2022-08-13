de4d7ef09ecb01fb06c76251f64960fd4760d19d
mandara11 <mandaram2345@gmail.com>
2022-08-13 21:57:35 +0530

    Craete a Stack

---
+++ Stack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class Stack
+    {
+        public static void CreateStack()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
